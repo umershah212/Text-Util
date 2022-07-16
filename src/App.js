2f3e30b3e7e0a269de9ee3e9b59abfab34933b4c
Umer <umershah212@gmail.com>
2022-07-17 00:48:59 +0500

    Added Textfrom in components and added a state variable in Textform

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import NavBar from "./components/NavBar";
 import "./App.css";
+import Textform from "./components/Textform";
 
 function App() {
   document.title = "Text Utils";
   return (
     <>
       <NavBar title="Text Utility" aboutText="About TextUtil" />
+      <div className="container my-3">
+        <Textform text="Enter Text Below" />
+      </div>
     </>
   );
 }
---
+++ src/components/Textform.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const Textform = (props) => {
+  const [text, setText] = useState();
+
+  const onHandleEvent = (event) => {
+    setText(event.target.value);
+  };
+  const onBtnClick = () => {
+    const temp = text.value;
+    setText(temp);
+    console.log("Upper case clicked");
+  };
+
+  return (
+    <>
+      <div className="mb-3">
+        <label htmlFor="myBox" className="form-label">
+          {props.text}
+        </label>
+      </div>
+      <textarea
+        className="form-control"
+        id="myBox"
+        rows="3"
+        placeholder="Enter the text here"
+        value={text}
+        onChange={onHandleEvent}
+      ></textarea>
+      <button className="btn btn-primary" onClick={onBtnClick}>
+        Convert to Uppercase
+      </button>
+    </>
+  );
+};
+
+Textform.propTypes = { text: PropTypes.string };
+
+export default Textform;
